62312569183837d21982c8e421da223736b5a348
Mhdsinaan <sinanmhd154@gmail.com>
2025-03-06 10:15:25 +0530

    7

--- 136.js
+++ 136.js
@@ -1,15 +1,7 @@
-var singleNumber = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let count = 0;
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                count++;
-            }
-        }
-        if (count === 1) {
-            return nums[i]; 
-        }
-    }
+var reverse = function(x) {
+    
+    return parseInt( x.toString().split("").reverse().join("")) * Math.sign(x);
 };
 
-console.log(singleNumber([2,2,1])); 
+const x = -123;
+console.log(reverse(x)); 
