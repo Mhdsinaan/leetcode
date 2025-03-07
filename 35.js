aa65b7fbb9282d2ee6e739f5879f1cf0e0b1a390
Mhdsinaan <sinanmhd154@gmail.com>
2025-03-07 10:19:21 +0530

    557

--- 35.js
+++
@@ -1,8 +0,0 @@
-function truncateSentence(s, k) {
-    const words = s.split(' ');
-    const truncatedWords = words.slice(0, k);
-    const result = truncatedWords.join(' ');
-
-    return result;
-}
-console.log(truncateSentence("sinan muhammed areekode",2));
---
+++ 557.js
@@ -0,0 +1,6 @@
+var reverseWords = function(s) {
+    var words = s.split(' ');
+    var reversedWords = words.map((word) => word.split('').reverse().join(''));
+    var result = reversedWords.join(' ');
+    return result;
+};
