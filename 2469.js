6538087521d217414a4568ba82c4687afc5ae45a
Mhdsinaan <sinanmhd154@gmail.com>
2025-03-15 10:04:06 +0530

    2469

---
+++ 2469.js
@@ -0,0 +1,10 @@
+
+ 
+var convertTemperature = function(celsius) {
+    let kelvin = celsius + 273.15;      
+    let fahrenheit = celsius * 1.80 + 32.00;
+
+    return [kelvin, fahrenheit]; 
+};
+console.log(convertTemperature(36.50));
+
