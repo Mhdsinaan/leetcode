a8465c9be0572c1d1ec6a1874485709fc13b2692
Mhdsinaan <sinanmhd154@gmail.com>
2025-03-27 10:14:00 +0530

    2833

---
+++ MyCSharpApp/2833.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Solution {
+    public int FurthestDistanceFromOrigin(string moves) {
+      int left=0;
+      int right=0;
+      int underScore=0;
+
+      foreach(var move in moves){
+        if(move=='L') left ++;
+        else if(move=='R') right++;
+        else if(move=='_') underScore++;
+
+      }
+       return Math.Abs(left - right) + underScore;
+    }
+    static void Main()
+    {
+      Solution Sol=new Solution();
+     
+      Console.WriteLine(Sol.FurthestDistanceFromOrigin("L_RL__R"));
+    }
+}
--- MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c359a7253171fa8c4f8953385e26caa8e17387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146ae610ee12bb7428ad47cbd6cf190e5e34ed42")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
