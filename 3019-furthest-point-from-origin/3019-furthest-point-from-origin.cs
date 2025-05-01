f6aedc230b59117c9b78e4ab2c689922497e6d9e
Mhdsinaan <sinanmhd154@gmail.com>
2025-05-02 00:25:30 +0530

    Time: 0 ms (100%), Space: 41.7 MB (32.14%) - LeetHub

---
+++ 3019-furthest-point-from-origin/3019-furthest-point-from-origin.cs
@@ -0,0 +1,17 @@
+
+public class Solution {
+    public int FurthestDistanceFromOrigin(string moves) {
+      int left=0;
+      int right=0;
+      int underScore=0;
+
+      foreach(var move in moves){
+        if(move=='L') left ++;
+        else if(move=='R') right++;
+        else if(move=='_') underScore++;
+
+      }
+       return Math.Abs(left - right) + underScore;
+    }
+    
+}
