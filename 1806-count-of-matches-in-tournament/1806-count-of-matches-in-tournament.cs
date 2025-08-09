d5b90d99c8c47b0e5db9091a25404feceaa08531
Mhdsinaan <sinanmhd154@gmail.com>
2025-08-09 11:13:12 +0530

    Time: 22 ms (81.82%), Space: 29.1 MB (29.55%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cs
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cs
@@ -1,7 +1,19 @@
 public class Solution {
     public int NumberOfMatches(int n) {
 
-         return n - 1;
+       int matches=0;
+       while(n>1){
+        if(n%2==0){
+            matches+=n/2;
+            n=n/2;
+
+        }else{
+            matches+=(n-1)/2;
+            n=(n-1)/2+1;
+            
+        }
+       }
+       return matches;
 
         
     }
